a355fa4af3082d7330a57001b26aeebb3cd7df22
Rodrigo Hernández <42217144+rodcko@users.noreply.github.com>
2021-04-11 21:23:23 -0600

    ReactDOM explication

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+// const element = document.createElement("h1");
+// element.innerText = "Hello, Rodcko Badges";
+
+// const container = document.getElementById("app");
+
+// container.appendChild(element);
+
 import React from "react";
 import ReactDOM from "react-dom";
 
+const element = <h1>Hola, Rodcko from !</h1>;
+
 const container = document.getElementById("app");
 
-const element = <h1>Hello, Rodcko Badges</h1>;
+// ReactDOM.render(__que__, __donde__);
 ReactDOM.render(element, container);
