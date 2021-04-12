be580e8e75ab51d4b6c72526d9b1fa53ff9ce983
Rodrigo Hernández <42217144+rodcko@users.noreply.github.com>
2021-04-11 22:14:18 -0600

    First component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const jsx = <h1>Hola, Rodcko from !</h1>;
-const element = React.createElement(
-  "a",
-  { href: "https://platzi.com" },
-  "Ir a Platzi"
-);
+import Badge from "./components/Badge";
+
 const container = document.getElementById("app");
 
-// ReactDOM.render(__que__, __donde__);
-ReactDOM.render(element, container);
+ReactDOM.render(<Badge />, container);
