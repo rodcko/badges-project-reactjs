32e9408b8b77b08df1a0de6018c47a8b6b20bd25
Rodrigo Hernández <42217144+rodcko@users.noreply.github.com>
2021-04-13 21:54:49 -0600

    Improvements of user interface with a layout

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 
 import "./styles/Badges.css";
 import confLogo from "../images/badge-header.svg";
-import Navbar from "../components/Navbar";
 import Badge from "../components/Badge";
 import BadgesList from "../components/BadgesList";
 
@@ -45,9 +44,7 @@ class Badges extends React.Component {
 
   render() {
     return (
-      <div>
-        <Navbar />
-
+      <React.Fragment>
         <div className="Badges">
           <div className="Badges__hero">
             <div className="Badges__container">
@@ -73,7 +70,7 @@ class Badges extends React.Component {
             </div>
           </div>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
